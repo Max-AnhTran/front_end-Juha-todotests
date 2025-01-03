6911b91ea8439bf8ea364453992cb54f69c1134b
Juha Hinkula <hinkula@gmail.com>
2025-01-03 11:16:09 +0200

    Update TodoTable.tsx
    Heaer added

--- src/TodoTable.tsx
+++ src/TodoTable.tsx
@@ -3,15 +3,23 @@ import { TodoTableProps } from "./types"
 export default function TodoTable(props: TodoTableProps) {
   return (
     <div>
-      <table id="todotable"><tbody>
-      {
-        props.todos.map((todo, index: number) => 
+      <table id="todotable">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Duedate</th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+        props.todos.map((todo: Todo, index: number) => 
           <tr key={index}>
             <td>{todo.date}</td>
             <td>{todo.description}</td>
           </tr>)
         }
-        </tbody></table>    
+        </tbody>
+      </table>    
     </div>
   )
 }
